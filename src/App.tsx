62bffc2436223daeb4a0678999f0320b5bf4f9f2
AyllaChristinne <ayllachristinne15@gmail.com>
2025-01-11 15:12:36 -0300

    feat: added theme context

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Details } from "./pages/Details";
 import { NotFound } from "./pages/NotFound";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Details />} />
-        <Route path="*" element={<NotFound />} /> */
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<Details />} />
+          <Route path="*" element={<NotFound />} /> */
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,47 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface IThemeContext {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<IThemeContext | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: PropsWithChildren) => {
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
+    const storedTheme = window.localStorage.getItem("@cubos-movies/theme");
+    if (storedTheme === "light" || storedTheme === "dark") {
+      return storedTheme;
+    }
+    return "dark";
+  });
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("color-theme", theme);
+    window.localStorage.setItem("@cubos-movies/theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div data-theme={theme}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
