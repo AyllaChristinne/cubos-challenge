b9f69afa35298117cac99cf94575de4e21a70e11
AyllaChristinne <ayllachristinne15@gmail.com>
2025-01-14 17:49:45 -0300

    feat: added NotFound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,3 +1,14 @@
+import { Link, useNavigate } from "react-router-dom";
+import "./index.scss";
+
 export const NotFound = () => {
-  return <div>NotFound works!</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="notFound_container">
+      <h1 className="notFound_text">Página não encontrada D:</h1>
+      <Link className="notFound_link" to="/">
+        Ir para página inicial
+      </Link>
+    </div>
+  );
 };
